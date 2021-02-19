d1a6a365acf3717a85362edf0cd835bc2f0e2aac
JaydenElliott <jayden.elliott@outlook.com>
2021-02-20 09:42:28 +1100

    BUILD: channel interaction interface.

---
+++ managers/models/channel.go
@@ -0,0 +1,16 @@
+package models
+
+// Obtain class attributes
+type Channel interface {
+	getID() string
+	getName() string
+	getUsers() string
+}
+
+// Interact with the channel class
+type ChannelLogic interface {
+	AddChannel(channel Channel)
+	RemoveChannel(channel Channel)
+	MergeChannel(channel Channel)
+	FindChannel(name string)
+}
