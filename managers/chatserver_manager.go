c99df540dcf4e196ecf1717a2411a8d37e356cc2
JaydenElliott <jayden.elliott@outlook.com>
2021-02-20 08:38:31 +1100

    BUILD: user struct details.

--- managers/chatserver_manager.go
+++ managers/chatserver_manager.go
@@ -1,13 +1,27 @@
 package managers
 
+/* Define Fundamental Types */
+
 // Struct that bundles and acts as a controller between the users and channels
 type ChatServerManager struct {
+	UserManager    *UserManager
+	ChannelManager *ChannelManager
 }
 
 // Handles all business logic relating to a User
 type UserManager struct {
+	users []*User
+}
+
+type User struct {
+	email       *string
+	displayName *string
+	channels    []*Channel // chat channels this user is apart of  ** REVIEW (would a map be better)
 }
 
 // Handles all business logic relating to a Channel
 type ChannelManager struct {
+}
+
+type Channel struct {
 }
