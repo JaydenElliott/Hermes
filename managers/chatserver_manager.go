2d2ac0bf302395786c6031363efaa061bf6c369a
JaydenElliott <jayden.elliott@outlook.com>
2021-02-20 08:27:55 +1100

    BUILD: files for managers.

---
+++ managers/chatserver_manager.go
@@ -0,0 +1,13 @@
+package managers
+
+// Struct that bundles and acts as a controller between the users and channels
+type ChatServerManager struct {
+}
+
+// Handles all business logic relating to a User
+type UserManager struct {
+}
+
+// Handles all business logic relating to a Channel
+type ChannelManager struct {
+}
