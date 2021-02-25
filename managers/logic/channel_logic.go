31e72a57f548871dd2abcac69a22d1c6052cef4f
JaydenElliott <jayden.elliott@outlook.com>
2021-02-25 17:59:30 +1100

    DOCS: channel logic function comments.

--- managers/logic/channel_logic.go
+++ managers/logic/channel_logic.go
@@ -7,18 +7,22 @@ type Channel struct {
 	threads     map[*Thread]bool
 }
 
+// Get Channel ID
 func (channel *Channel) getID() string {
 	return channel.channelID
 }
 
+// Get Channel name
 func (channel *Channel) getName() string {
 	return channel.channelName
 }
 
+// Parameters for getUsers function
 type getUsersParams struct {
-	returnType string
+	returnType string // [id, username]
 }
 
+// Gets all users in a channel
 func (channel *Channel) getUsers(p getUsersParams) []string {
 	var users []string
 	for user, value := range channel.users {
