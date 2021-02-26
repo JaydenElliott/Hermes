c6be77abcac3693092f8c4e4d4ff201751c934e3
JaydenElliott <jayden.elliott@outlook.com>
2021-02-27 09:56:21 +1100

    BUILD: parameter files for user and channel.

--- managers/logic/userParameters.go
+++ managers/logic/userParameters.go
@@ -1,6 +1,6 @@
 package logic
 
 // Parameters for getUsers function (see channel_logic.go and thread_logic.go)
-type getUsersParams struct {
-	returnType string // [userId, username]
+type GetUsersParams struct {
+	ReturnType string // [userId, username
 }
