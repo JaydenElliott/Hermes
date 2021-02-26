908a5d4aa4baf63a81746b219d8bac770863392e
JaydenElliott <jayden.elliott@outlook.com>
2021-02-27 10:13:14 +1100

    BUILD: create thread function and parameter specification.

--- managers/logic/thread_logic.go
+++ managers/logic/thread_logic.go
@@ -3,12 +3,19 @@ package logic
 import (
 	"errors"
 	"fmt"
+	"github.com/google/uuid"
 )
 
 type Thread struct {
 	threadID *string
 	users    map[*User]bool
 	channel  *Channel
+}
+
+func (thread *Thread) CreateThread(p CreateThread_) *Thread {
+	threadID := uuid.New().String()
+	users := make(map[*User]bool)
+	return &Thread{&threadID, users, &p.Channel}
 }
 
 // Get Thread ID
