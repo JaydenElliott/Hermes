24ee2b7314a284cae5a753ce5a74cd7f5aeb0ae1
JaydenElliott <jayden.elliott@outlook.com>
2021-02-27 10:00:01 +1100

    BUILD: createChannel method.

--- managers/logic/channel_logic.go
+++ managers/logic/channel_logic.go
@@ -3,6 +3,7 @@ package logic
 import (
 	"errors"
 	"fmt"
+	"github.com/google/uuid"
 )
 
 type Channel struct {
@@ -10,6 +11,14 @@ type Channel struct {
 	channelName *string
 	users       map[*User]bool
 	threads     map[*Thread]bool
+}
+
+// Create Channel
+func (channel *Channel) createChannel(p CreateChannel_) *Channel {
+	channelID := uuid.New().String() // generate channel uuid
+	users := make(map[*User]bool)
+	threads := make(map[*Thread]bool)
+	return &Channel{&channelID, &p.ChannelName, users, threads}
 }
 
 // Get Channel ID
@@ -23,21 +32,21 @@ func (channel *Channel) getName() *string {
 }
 
 // Description:    Gets all users in a specific Channel.
-// Input:          getUsersParams struct (logicParameters.go).
+// Input:          getUsersParams struct (userParameters.go).
 // Returns:        List of pointers to user username or userID and error.
-func (channel *Channel) getChannelUsers(p getUsersParams) ([]*string, error) {
+func (channel *Channel) getChannelUsers(p GetUsersParams) ([]*string, error) {
 	var users []*string
 	var errorMsg error = nil
 
 	// Loop through and append to return array all users satisfying users: True
 	for user, value := range channel.users {
 		if value {
-			if p.returnType == "username" {
+			if p.ReturnType == "username" {
 				users = append(users, user.username)
-			} else if p.returnType == "userId" {
+			} else if p.ReturnType == "userId" {
 				users = append(users, user.userId)
 			} else {
-				errorMsg = errors.New(fmt.Sprintf("Invalid getChannelUsers() input parameter: %s", p.returnType))
+				errorMsg = errors.New(fmt.Sprintf("Invalid getChannelUsers() input parameter: %s", p.ReturnType))
 				users = nil
 				break
 			}
