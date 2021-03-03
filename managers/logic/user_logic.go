8623df88d647e3c35f085ae4e6a7c034dfbf6af4
wjjmjh <u6269649@anu.edu.au>
2021-03-03 21:16:27 +1100

    BUILD: prototype of CircularRead in user.

--- managers/logic/user_logic.go
+++ managers/logic/user_logic.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	"log"
+	"time"
 )
 
 type User struct {
@@ -14,6 +16,16 @@ type User struct {
 	conn       *websocket.Conn
 	wsServer   *WsServer
 	dataBuffer chan []byte
+}
+
+// Conn default:
+// maxMessageSize: 1000
+// duration: 60 * time.Second
+type Conn struct {
+	// Maximum message size allowed from peer.
+	maxMessageSize int64
+	// Max time till next pong from peer
+	duration time.Duration
 }
 
 // Create user method -> Used by user_manager.go
@@ -48,12 +60,47 @@ func (user *User) GetWsSever() *WsServer {
 	return user.wsServer
 }
 
+func (user *User) configureConn(maxMessageSize int64, duration time.Duration) {
+
+	if maxMessageSize == 0 {
+		maxMessageSize = 10000
+	}
+
+	if duration == 0 {
+		duration = 60 * time.Second
+	}
+
+	user.conn.SetReadLimit(maxMessageSize)
+	_ = user.conn.SetReadDeadline(time.Now().Add(duration))
+	user.conn.SetPongHandler(func(string) error { _ = user.conn.SetReadDeadline(time.Now().Add(duration)); return nil })
+}
+
+func (user *User) CircularRead(maxMessageSize int64, duration time.Duration) {
+	defer func() {
+		_ = user.DisconnectWithWsServer()
+	}()
+
+	user.configureConn(maxMessageSize, duration)
+
+	// Start endless read loop, waiting for messages from client
+	for {
+		_, jsonMessage, err := user.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("[ERROR] unexpected close error: %v", err)
+			}
+			break
+		}
+		user.wsServer.broadcast <- jsonMessage
+	}
+}
+
 func (user *User) DisconnectWithWsServer() error {
 	user.wsServer.unregister <- user
 	close(user.dataBuffer)
 	err := user.conn.Close()
 	if err != nil {
-		return fmt.Errorf("[Error] unable to disconnect user %s with the ws server", *user.GetUsername())
+		return fmt.Errorf("[ERROR] unable to disconnect user %s with the ws server", *user.GetUsername())
 	}
 	return nil
 }
