0c44ad0af51b34382d3ed826f767e54dd50c41d5
JaydenElliott <jayden.elliott@outlook.com>
2021-03-05 07:38:15 +1100

    ENH: let UpgrateHTTPToWS handle initialising upgrader.

--- pkg/util/connection/connection.go
+++ pkg/util/connection/connection.go
@@ -14,8 +14,18 @@ type BufferSizes struct {
 	writeBufferSize int
 }
 
-//makeUpgrader: make a websocket upgrader based on specified buffersizes.
-func makeUpgrader(bufferSizes *BufferSizes) websocket.Upgrader {
+// Creates buffers with default (4096/4096) read write sizes
+func CreateDefaultBuffer() *BufferSizes {
+	return &BufferSizes{4096, 4096}
+}
+
+// Creates buffers with custom read/write sizes
+func CreateCustomBuffer(readSize int, writeSize int) *BufferSizes {
+	return &BufferSizes{readSize, writeSize}
+}
+
+//MakeUpgrader: make a websocket upgrader based on specified buffersizes.
+func MakeUpgrader(bufferSizes *BufferSizes) websocket.Upgrader {
 
 	if bufferSizes.readBufferSize == 0 {
 		bufferSizes.readBufferSize = 4096
@@ -32,7 +42,7 @@ func makeUpgrader(bufferSizes *BufferSizes) websocket.Upgrader {
 }
 
 // upgradeHTTPToWS upgrades the HTTP server connection to the WebSocket protocol.
-func upgradeHTTPToWS(upgrader websocket.Upgrader, w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+func UpgradeHTTPToWS(upgrader websocket.Upgrader, w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return nil, err
