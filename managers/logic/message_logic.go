a1644a1e80eb2d5b34a667ad81fccbaed86a679c
JaydenElliott <jayden.elliott@outlook.com>
2021-03-11 07:20:28 +1100

    BUILD: message marshal method for Message type.

--- managers/logic/message_logic.go
+++ managers/logic/message_logic.go
@@ -1,5 +1,10 @@
 package logic
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type Message struct {
 	// Message request type
 	Action string `json:"action"`
@@ -8,8 +13,17 @@ type Message struct {
 	Message string `json:"message"`
 
 	// Message target (room or user)
-	Target string `json:"target"`
+	Target *Channel `json:"target"`
 
 	// User sending the message
 	Sender *User `json:"sender"`
+}
+
+func (message *Message) marshal() []byte {
+	_json, err := json.Marshal(message)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return _json
 }
